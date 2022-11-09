052e633b4cee0a941b4e648ebfda20e4cb319944
luong <luong@example.com>
2022-11-09 09:02:32 +0700

    validation form register, addAccount, EditAccount

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import { useFormik } from 'formik'
+import * as Yup from "yup";
 import { useDispatch } from 'react-redux';
 import { RegisterAction } from './../../redux/Actions/ManageUserAction';
 import { NavLink } from 'react-router-dom';
@@ -15,7 +16,14 @@ export default function Register(props) {
             Password: '',
             Role: "CLIENT"
         },
+        validationSchema: Yup.object({
+            Username: Yup.string()
+                .required("Không được trống !"),
+            Password: Yup.string()
+                .min(6, "Tối thiểu 6 kí tự")
+                .required("Không được trống !"),
 
+        }),
         onSubmit: values => {
             dispatch(RegisterAction(values))
         },
@@ -36,10 +44,16 @@ export default function Register(props) {
                                 <div className="mb-6">
                                     <span className='text-xl'>Tài khoản:</span>
                                     <input type="text" name='Username' onChange={formik.handleChange} className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Nhập tài khoản..." />
+                                    {formik.errors.Username && formik.touched.Username && (
+                                        <p className='m-0 mt-1 text-red-600'>{formik.errors.Username}</p>
+                                    )}
                                 </div>
                                 <div className="mb-6">
                                     <span className='text-xl'>Mật khẩu:</span>
                                     <input type="password" name='Password' onChange={formik.handleChange} className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Nhập mật khẩu..." />
+                                    {formik.errors.Password && formik.touched.Password && (
+                                        <p className='m-0 mt-1 text-red-600'>{formik.errors.Password}</p>
+                                    )}
                                 </div>
 
                                 <div className="text-center lg:text-left">

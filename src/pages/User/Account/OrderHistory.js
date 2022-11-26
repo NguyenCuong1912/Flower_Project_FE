088275583f1fa74180a69d9454164a6bf454a159
luong <luong@example.com>
2022-11-26 08:33:30 +0700

    update

--- src/pages/User/Account/OrderHistory.js
+++ src/pages/User/Account/OrderHistory.js
@@ -1,46 +1,43 @@
-import { Table } from 'antd'
 import moment from 'moment';
 import React, { Fragment, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { history } from '../../../App';
-import { GetListCheckout } from '../../../redux/Actions/CheckoutAction';
+import { GetCheckoutHistory } from '../../../redux/Actions/CheckoutAction';
 import { DOMAIN } from '../../../utils/Settings/config';
-import { _admin, _bill, _detail } from '../../../utils/util/ConfigPath';
 
 export default function OrderHistory() {
     const dispatch = useDispatch();
 
     const { cartHistory } = useSelector(state => state.ManageCartReducer);
-    console.log(cartHistory)
     useEffect(() => {
-        dispatch(GetListCheckout())
+        dispatch(GetCheckoutHistory())
     }, [])
 
     const renderTable = () => {
         return cartHistory.map((item, index) => {
-            return <div className='border px-2'>
-                <span>Hóa đơn { item.Bill.id }</span> - <span>Ngày thanh toán : { moment(item.Bill.createdAt).format('DD/MM/YYYY') }</span>
-                { item.Detail.map((detail, indexD) => {
-                    return <div className='flex justify-between border p-3 items-center my-2'>
+            console.log('first', item)
+            return <div className='border px-2' key={index}>
+                <span>Hóa đơn {item.Bill.id}</span> - <span>Ngày thanh toán : {moment(item.Bill.createdAt).format('DD/MM/YYYY')}</span>
+                {item.Detail.map((detail, indexD) => {
+                    return <div key={indexD} className='flex justify-between border p-3 items-center my-2'>
                         <div className='w-36'>
-                            <img src={ `${DOMAIN}/${detail.Product.ProductImage}` } alt='123' />
+                            <img src={`${DOMAIN}/${detail.Product.ProductImage}`} alt='123' />
                         </div>
-                        <p>{ detail.Product.ProductName }</p>
-                        <p>{ detail.Product.GroupFlower.GroupName }</p>
-                        <div>
-                            <p>{ detail.Price }</p>
-                            <span>x{ detail.Quantity }</span>
+                        <p>{detail.Product.ProductName}</p>
+                        <p>{detail.Product.GroupFlower.GroupName}</p>
+                        <div className='flex'>
+                            <p>{(detail.Price * 1).toLocaleString()} <span className='underline mr-4'>đ</span></p>
+                            <span>x{detail.Quantity}</span>
                         </div>
-                        <p>{ detail.Product.Discount }%</p>
-                        <p>{ detail.Price * detail.Quantity }</p>
+                        <p>{detail.Product.Discount}%</p>
+                        <p className='text-base font-medium text-red-500'>{((detail.Price - detail.Price * detail.Product.Discount / 100) * detail.Quantity).toLocaleString()} <span className='underline mr-4'>đ</span></p>
 
                     </div>
-                }) }
+                })}
                 <div className='flex justify-end text-right'>
                     <div>
                         <span>Tổng tiền: </span> <span>150000đ</span>
                         <div>
-                            <span>Ngày thanh toán : </span> <span>20/11</span>
+                            <span>Ngày thanh toán : </span> <span>{moment(item.Bill.createdAt).format('DD/MM/YYYY')}</span>
                         </div>
                     </div>
                 </div>
@@ -94,9 +91,9 @@ export default function OrderHistory() {
             <div className='grid grid-cols-5'>
                 <div className='col-start-2 col-span-3 mt-4'>
                     <h2 className='text-4xl font-bold text-center text-red-500'>Lịch sử đặt hàng</h2>
-                    {/* <Table dataSource={ cartHistory } columns={ columns } rowKey='id' /> */ }
+                    {/* <Table dataSource={ cartHistory } columns={ columns } rowKey='id' /> */}
                     <div className='border p-3'>
-                        { renderTable() }
+                        {renderTable()}
                     </div>
                 </div>
             </div>
